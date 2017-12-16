32b07b7cfe3b511a2e8fc1fc1ee224a2c8f59596
luiyezheng <luiyezheng@126.com>
2017-12-16 17:57:38 +0800

    Initial Commit

---
+++ snkcooker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  snkcooker
+//
+//  Created by 刘业臻 on 2017/12/16.
+//  Copyright © 2017年 luiyezheng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ snkcooker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  snkcooker
+//
+//  Created by 刘业臻 on 2017/12/16.
+//  Copyright © 2017年 luiyezheng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
