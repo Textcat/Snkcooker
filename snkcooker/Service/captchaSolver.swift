2027b003c4557be907bf112fadf71af89dc0a057
luiyezheng <luiyezheng@126.com>
2017-12-27 19:38:28 +0800

    Rewrite captchaSolver with Alamofire

--- snkcooker/Service/captchaSolver.swift
+++ snkcooker/Service/captchaSolver.swift
@@ -7,72 +7,51 @@
 //
 
 import Foundation
+import Alamofire
 
-public class CapSolver {
-    let site_key : String
-    let auth_key : String
-    var task_id : Int?
+internal class CapSolver {
+    let siteKey = "6LeoeSkTAAAAAA9rkZs5oS82l69OEYjKRZAiKdaF"
+    let authKey = "c2fd8111e8359547c83c56c657ade515"
+    var taskID : Int?
     var pUrl : String
     var gresponse : String?
     
-    
-    init?(site_key:String, pUrl:String, auth_key:String) {
-        self.site_key = site_key
-        self.auth_key = auth_key
+    init?(pUrl:String) {
         self.pUrl = pUrl
     }
     
+    internal func startSolve(completion:@escaping (_ solution:String) -> Void) {
+        let url = "https://api.anti-captcha.com/createTask"
+        let postData:[String:Any] = ["clientKey":authKey, "task":["type":"NoCaptchaTaskProxyless","websiteURL":pUrl, "websiteKey":siteKey]]
+        
+        Alamofire.request(url, method: .post, parameters: postData, encoding: JSONEncoding.default).responseJSON{response in
+            if let json = response.result.value as? [String:Any] {
+                self.taskID = json["taskId"] as? Int
+                self.getResult(completion: completion)
+            }
+        }
+    }
     
-    public func getRespose(){
-        let post_data:[String:Any] = ["clientKey":self.auth_key, "task":["type":"NoCaptchaTaskProxyless","websiteURL":self.pUrl, "websiteKey":self.site_key]]
-        let request = Utility.genPostRequest(from: URL(string: "https://api.anti-captcha.com/createTask")!, with: post_data)
-        let data_task = URLSession.shared.dataTask(with: request!) {(data, response, error) in
-            if let data = data {
-                do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: [])
-                    if let object = json as? [String: Any] {
-                        self.task_id = object["taskId"] as? Int
-                        self.solveCap()
-                    }
-                }catch {
-                    print(error)
+    private func getResult(completion:@escaping (_ solution:String) -> Void) {
+        guard let id = taskID else {return}
+        let postData:[String:Any] = ["clientKey":authKey,
+                                     "taskId":id]
+        
+        let url = "https://api.anti-captcha.com/getTaskResult"
+        
+        Alamofire.request(url, method: .post, parameters: postData, encoding: JSONEncoding.default).responseJSON{response in
+            if let json = response.result.value as? [String:Any] {
+                if Array(json.keys).contains("solution"),let solution:[String:String] = json["solution"] as? [String:String] {
+                    guard let response = solution["gRecaptchaResponse"] else {return}
+                    completion(response)
+                }else {
+                    print(json)
+                    DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(1), execute: {
+                        self.getResult(completion: completion)
+                    })
                 }
             }
         }
-        data_task.resume()
-    }
-    
-    
-    private func postRequest(with request:URLRequest) {
-        _ = URLSession.shared.dataTask(with: request) {(data, response, error) in
-            
-            if let data = data {
-                do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: [])
-                    if let object = json as? [String: Any] {
-                        if Array(object.keys).contains("solution") {
-                            let solution:[String:String] = object["solution"] as! [String:String]
-                            self.gresponse = solution["gRecaptchaResponse"]!
-                        }else {
-                            print(object)
-                            DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(2), execute: {
-                                self.postRequest(with: request)
-                            })
-                        }
-                    }
-                }catch {
-                    print(error)
-                }
-            }
-            }.resume()
-    }
-    
-    
-    private func solveCap(){
-        let postData:[String:Any] = ["clientKey":self.auth_key,
-                                        "taskId":self.task_id!]
-        let request = Utility.genPostRequest(from: URL(string:"https://api.anti-captcha.com/getTaskResult")!, with: postData)
-        self.postRequest(with: request!)
     }
 }
 
